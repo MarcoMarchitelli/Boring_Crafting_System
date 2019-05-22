26b78e8efaa8e09406be8230b1478fdb8f66425f
Marco Marchitelli <marchitelli97@libero.it>
2019-05-22 16:27:40 +0200

    Removed Statemachine (for noobs). Added dotween (for pros). Making the boring less boring with some cactus teachings.

---
+++ Assets/Scripts/Editor/ImageTweenerEditor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEditor;
+
+//[CustomEditor(typeof(ImageTweener))]
+//[CanEditMultipleObjects]
+public class ImageTweenerEditor : Editor
+{
+    bool color = false;
+    bool rotation = false;
+    bool rotationShake = false;
+
+    SerializedProperty tweenOnAwake, tweenColor, gradient, colorSpeed, shakeRotation, shakeAngles, shakeSpeed,
+                       shakeVibration, shakeElasticity, tweenScale, image, text;
+
+    private void OnEnable()
+    {
+        tweenOnAwake = serializedObject.FindProperty("tweenOnAwake");
+        tweenColor = serializedObject.FindProperty("tweenColor");
+        gradient = serializedObject.FindProperty("gradient");
+        colorSpeed = serializedObject.FindProperty("colorSpeed");
+        shakeRotation = serializedObject.FindProperty("shakeRotation");
+        shakeAngles = serializedObject.FindProperty("shakeAngles");
+        shakeSpeed = serializedObject.FindProperty("shakeSpeed");
+        shakeVibration = serializedObject.FindProperty("shakeVibration");
+        shakeElasticity = serializedObject.FindProperty("shakeElasticity");
+        tweenScale = serializedObject.FindProperty("tweenScale");
+        image = serializedObject.FindProperty("image");
+        text = serializedObject.FindProperty("text");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(tweenOnAwake);
+        EditorGUILayout.Space();
+
+        tweenColor.boolValue = EditorGUILayout.ToggleLeft("Color", tweenColor.boolValue, EditorStyles.boldLabel);
+        if (tweenColor.boolValue)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(gradient);
+            EditorGUILayout.PropertyField(colorSpeed);
+            EditorGUI.indentLevel--;
+        }
+
+        rotation = EditorGUILayout.ToggleLeft("Rotation", rotation, EditorStyles.boldLabel);
+        if (rotation)
+        {
+            EditorGUI.indentLevel++;
+            rotationShake = EditorGUILayout.ToggleLeft("Shake", rotationShake, EditorStyles.boldLabel);
+            if (rotationShake)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(shakeAngles);
+                EditorGUILayout.PropertyField(shakeSpeed);
+                EditorGUILayout.PropertyField(shakeVibration);
+                EditorGUILayout.PropertyField(shakeElasticity);
+                EditorGUI.indentLevel--;
+            }
+            EditorGUI.indentLevel--;
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Tween Targets", EditorStyles.boldLabel);
+        EditorGUI.indentLevel++;
+        EditorGUILayout.PropertyField(image);
+        EditorGUILayout.PropertyField(text);
+        EditorGUI.indentLevel--;
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/ImageTweener.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class ImageTweener : MonoBehaviour
+{
+    public bool tweenOnAwake;
+
+    [Header("Color")]
+    public bool tweenColor;
+    public Gradient gradient;
+    public float colorSpeed;
+
+    [Header("Rotation")]
+    public bool shakeRotation;
+    public Vector3 shakeAngles = Vector3.forward * 20;
+    public float shakeSpeed = .7f;
+    public int shakeVibration = 10;
+    [Range(0, 1)]
+    public float shakeElasticity = 1;
+
+    [Header("Tween Targets")]
+    public Image image;
+    public TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        if (tweenOnAwake)
+        {
+            StartTweening();
+        }
+    }
+
+    public void StartTweening()
+    {
+        if (tweenColor)
+        {
+            if (image)
+            {
+                image.DOGradientColor(gradient, colorSpeed).SetLoops(-1);
+            }
+            if (text)
+            {
+                text.fontMaterial.DOGradientColor(gradient, "_FaceColor", colorSpeed).SetLoops(-1);
+            }
+        }
+
+        if (shakeRotation)
+        {
+            image.rectTransform.DOPunchRotation(shakeAngles, shakeSpeed, shakeVibration, shakeElasticity).SetLoops(-1);
+        }
+    }
+
+    public void HandlePointerEnter()
+    {
+        image.rectTransform.DOScale(Vector3.one * 1.5f, .2f);
+    }
+
+    public void HandlePointerExit()
+    {
+        image.rectTransform.DOScale(Vector3.one, .2f);
+    }
+
+    public void HandlePointerDown()
+    {
+        image.rectTransform.DOPunchScale(Vector3.one * 2f, .2f, 5, 1);
+    }
+}
